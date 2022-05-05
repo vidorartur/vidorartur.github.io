25add12a9d8d8eb90ea7db39e127aa1402620860
Artur Vidor <vidorartur@gmail.com>
2022-05-05 14:00:16 -0300

    adicionando contato

---
+++ contato/script.js
@@ -0,0 +1,31 @@
+function navfixed(event) {
+    const nav = document.querySelector('.nav-list')
+
+
+    if (event.deltaY < 0) {
+        nav.style.visibility = "visible";
+    } else if (event.deltaY > 0) {
+        nav.style.visibility = "hidden";
+    }
+}
+
+function hiddenBar() {
+    const hidden = document.querySelector('.hiddenBar')
+    hidden.style.visibility = 'visible'
+}
+
+function closeBar() {
+    const hidden = document.querySelector('.hiddenBar')
+    hidden.style.visibility = 'hidden'
+}
+
+const buttonHam = document.querySelector('#ham-bar')
+const buttonX = document.querySelector('#butn-X')
+const hidden = document.querySelector('.hiddenBar')
+
+buttonX.addEventListener('click', closeBar)
+buttonHam.addEventListener('click', hiddenBar)
+hidden.addEventListener('mouseleave', closeBar)
+
+
+window.addEventListener('wheel', navfixed);
