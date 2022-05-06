37fd19c9aa797adc66be8a5345904df2dff41876
Artur Vidor <vidorartur@gmail.com>
2022-05-06 13:39:02 -0300

    Arrumando a casa 2

--- contato/script.js
+++ contato/script.js
@@ -19,10 +19,18 @@ function closeBar() {
     hidden.style.visibility = 'hidden'
 }
 
+const counter = document.querySelector('#counter');
+const textarea = document.querySelector('#textarea')
+
+function contador(element){
+    const valorAtual = element.target.value.length;
+    counter.innerText = 500 - valorAtual;
+}
+
 const buttonHam = document.querySelector('#ham-bar')
 const buttonX = document.querySelector('#butn-X')
 const hidden = document.querySelector('.hiddenBar')
-
+textarea.addEventListener('input', contador);
 buttonX.addEventListener('click', closeBar)
 buttonHam.addEventListener('click', hiddenBar)
 hidden.addEventListener('mouseleave', closeBar)
